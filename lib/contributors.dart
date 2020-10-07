697ce9d91ea8366e0d203773161322fd9fc028e6
Mahar zeeshan <mahar.zeeshan5994@gmail.com>
2020-10-07 22:13:43 +0500

    Update contributors.dart

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -10,11 +10,13 @@ class Contributor extends StatelessWidget {
   // < Add your Full Name in the List[] >
   final contributorsName = [
     'Muhammad Hamza',
+    'Muhammad Zeeshan',
   ];
 
   // < Add your GitHub Username in the List[] >
   final gitHubUserName = [
     'm-hamzashakeel',
+    'zeeshanmahar007',
   ];
 
   @override
