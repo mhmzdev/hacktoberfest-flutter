4882718a2240fefbef7c6a3fa371f3f16566607a
m-hamzashakeel <hamza.6.shakeel@gmail.com>
2020-10-07 17:17:50 +0500

    inital settings

---
+++ lib/contributors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Contributor extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hacktober Fest - Contributor"),
+      ),
+      body: Center(
+        child: Text("Contributor List wil be shown here..!"),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:hacktoberfest_flutter/welcome.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Welcome(),
+    );
+  }
+}
