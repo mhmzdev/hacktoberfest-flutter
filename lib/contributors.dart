ed931d4ecd0d39b5754c9d980bf5affc5868fc00
SafuRaja7 <rajasaifi49@gmail.com>
2020-10-08 16:17:25 +0500

    added myself

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -10,11 +10,13 @@ class Contributor extends StatelessWidget {
   // < Add your Full Name in the List[] >
   final contributorsName = [
     'Muhammad Hamza',
+    'Muhammad Saif Waheed Raja'
   ];
 
   // < Add your GitHub Username in the List[] >
   final gitHubUserName = [
     'm-hamzashakeel',
+    'SafuRaja7'
   ];
 
   @override
