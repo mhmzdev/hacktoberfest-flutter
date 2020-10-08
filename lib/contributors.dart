06859cd6a891c16b8bf9d9b1f5c86e22ea3b2225
mayukhsil <mayukhsil@svceeng.edu.in>
2020-10-08 13:10:23 +0530

    added myself

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -8,14 +8,10 @@ import 'package:flutter/material.dart';
 
 class Contributor extends StatelessWidget {
   // < Add your Full Name in the List[] >
-  final contributorsName = [
-    'Muhammad Hamza',
-  ];
+  final contributorsName = ['Muhammad Hamza', 'Mayukh Sil'];
 
   // < Add your GitHub Username in the List[] >
-  final gitHubUserName = [
-    'm-hamzashakeel',
-  ];
+  final gitHubUserName = ['m-hamzashakeel', 'mayukhsil'];
 
   @override
   Widget build(BuildContext context) {
