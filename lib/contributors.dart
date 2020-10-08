33676a850c092fbaa12a4c90ef7290919e669e81
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2020-10-08 11:29:46 +0530

    added myself

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -10,11 +10,13 @@ class Contributor extends StatelessWidget {
   // < Add your Full Name in the List[] >
   final contributorsName = [
     'Muhammad Hamza',
+    'Muhammad Tabish Khanday',
   ];
 
   // < Add your GitHub Username in the List[] >
   final gitHubUserName = [
     'm-hamzashakeel',
+    'mtabishk',
   ];
 
   @override
