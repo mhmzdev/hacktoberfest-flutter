88cd78f42aff136c79c5740127d5add8d21f72fa
Yudi Purwanto <purwantoyudi42@gmail.com>
2020-10-08 22:00:04 +0700

    added myself

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -10,11 +10,13 @@ class Contributor extends StatelessWidget {
   // < Add your Full Name in the List[] >
   final contributorsName = [
     'Muhammad Hamza',
+    'Yudi Purwanto',
   ];
 
   // < Add your GitHub Username in the List[] >
   final gitHubUserName = [
     'm-hamzashakeel',
+    'zhiephie',
   ];
 
   @override
