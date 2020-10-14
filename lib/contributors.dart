bfc82c3b41dbd5dcec971e59a204f7d86df6d941
Apoorv-cloud <63242652+Apoorv-cloud@users.noreply.github.com>
2020-10-14 20:47:16 +0530

    Added my details

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -9,12 +9,12 @@ import 'package:flutter/material.dart';
 class Contributor extends StatelessWidget {
   // < Add your Full Name in the List[] >
   final contributorsName = [
-    'Muhammad Hamza',
+    'Apoorv Maheshwari',
   ];
 
   // < Add your GitHub Username in the List[] >
   final gitHubUserName = [
-    'm-hamzashakeel',
+    'Apoorv-cloud',
   ];
 
   @override
