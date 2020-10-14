a746e3efba5621217a05154add5672053045bcc9
Oussama Bouchikhi <45850487+oussamabouchikhi@users.noreply.github.com>
2020-10-14 14:07:35 +0100

    ✨ add my self

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -10,11 +10,13 @@ class Contributor extends StatelessWidget {
   // < Add your Full Name in the List[] >
   final contributorsName = [
     'Muhammad Hamza',
+    'Oussama Bouchikhi'
   ];
 
   // < Add your GitHub Username in the List[] >
   final gitHubUserName = [
     'm-hamzashakeel',
+    'oussamabouchikhi'
   ];
 
   @override
