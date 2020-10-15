320175da47b36aadc78c38e82cb736745d76b8b1
Amit Verma <ava.amitverma@gmail.com>
2020-10-16 01:29:58 +0530

    Update contributors.dart

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -10,11 +10,13 @@ class Contributor extends StatelessWidget {
   // < Add your Full Name in the List[] >
   final contributorsName = [
     'Muhammad Hamza',
+    'Amit Verma'
   ];
 
   // < Add your GitHub Username in the List[] >
   final gitHubUserName = [
     'm-hamzashakeel',
+    'amitverma-cf'
   ];
 
   @override
