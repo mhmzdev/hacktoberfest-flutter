1b4ec8d2ddc1f4eda7a481127a93c9f32be7c23e
Enzo Danjour <enzo.lops999@gmail.com>
2020-10-15 13:52:56 -0300

    Add myself

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -10,11 +10,13 @@ class Contributor extends StatelessWidget {
   // < Add your Full Name in the List[] >
   final contributorsName = [
     'Muhammad Hamza',
+    'Enzo Danjour'
   ];
 
   // < Add your GitHub Username in the List[] >
   final gitHubUserName = [
     'm-hamzashakeel',
+    'enzodanjour'
   ];
 
   @override
