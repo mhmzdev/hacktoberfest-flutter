4c60a2461b2d98ecf44daded571ce93144d3aecb
Amaldev M V <36617401+AmaldevAD@users.noreply.github.com>
2020-10-17 23:30:10 +0530

    Update contributors.dart

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -9,12 +9,12 @@ import 'package:flutter/material.dart';
 class Contributor extends StatelessWidget {
   // < Add your Full Name in the List[] >
   final contributorsName = [
-    'Muhammad Hamza',
+    'Amaldev M V',
   ];
 
   // < Add your GitHub Username in the List[] >
   final gitHubUserName = [
-    'm-hamzashakeel',
+    'AmaldevAD',
   ];
 
   @override
