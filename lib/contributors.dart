326bd1fae82ca2004c84a49b8195c50fa1ff76d6
Praneeth Fernando <praneethfdo@gmail.com>
2020-10-19 07:32:42 +0530

    updated contributors and participants

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -8,14 +8,10 @@ import 'package:flutter/material.dart';
 
 class Contributor extends StatelessWidget {
   // < Add your Full Name in the List[] >
-  final contributorsName = [
-    'Muhammad Hamza',
-  ];
+  final contributorsName = ['Muhammad Hamza', 'Praneeth Fernando'];
 
   // < Add your GitHub Username in the List[] >
-  final gitHubUserName = [
-    'm-hamzashakeel',
-  ];
+  final gitHubUserName = ['m-hamzashakeel', 'praneethfdo'];
 
   @override
   Widget build(BuildContext context) {
