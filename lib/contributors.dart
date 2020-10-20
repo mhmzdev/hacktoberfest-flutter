705691f0064316021de46789fb4d7e5d905f90f4
Md Zaid Ansari <zaidansari51097@gmail.com>
2020-10-20 11:14:47 +0530

    added razorcalhn

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -42,7 +42,8 @@ class Contributor extends StatelessWidget {
     'Aditya Birangal',
     'Sai Nitesh',
     'Muhammad Zeeshan',
-    'Praneeth Fernando'
+    'Praneeth Fernando',
+    'Md Zaid Ansari'
   ];
 
   // < Add your GitHub Username in the List[] >
@@ -79,7 +80,8 @@ class Contributor extends StatelessWidget {
     'AdityaBirangal',
     'sainiteshb',
     'zeeshanmahar007',
-    'praneethfdo'
+    'praneethfdo',
+    'razorcalhn'
   ];
   
   @override
