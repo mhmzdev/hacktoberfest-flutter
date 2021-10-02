dd98701e9568c6872b4c6350f179f173089259b4
wmfadel <mohamed.fadel24@hotmail.com>
2021-10-02 23:34:21 +0200

    remove unused import

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:hacktoberfest_flutter/animations/bottom_animation.dart';
 import 'package:hacktoberfest_flutter/util/restart_app.dart';
