38ffa8f03d8af682d76e9eb7aa5d2a723c2d8bb5
Shreyasrana18 <srana1_be20@thapar.edu>
2021-10-03 03:56:48 +0530

    Added myself

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -92,8 +92,9 @@ class Contributor extends StatelessWidget {
     'Zwe Mun Htun',
     'Srishti Vashistha',
     'Neha Ajith',
-    'Naveena Ramesh Vardhini '
-    'Muhammad Hilmy AL Jauzy'
+    'Naveena Ramesh Vardhini ',
+    'Muhammad Hilmy AL Jauzy',
+    'Shreyas Rana'
   ];
 
   final gitHubUserName = [
@@ -178,8 +179,9 @@ class Contributor extends StatelessWidget {
     'zwe-mun-htun',
     'srishtiv27',
     'neha-ajith',
-    'naveeramesh'
-        'hilmykun'
+    'naveeramesh',
+        'hilmykun',
+        'Shreyasrana18'
   ];
 
   @override
