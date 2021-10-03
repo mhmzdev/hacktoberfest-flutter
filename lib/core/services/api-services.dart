2f3e151da3c5cec2a4d2d1786c221b372d9cc912
Uzair Leo <uzairleo@Uzairs-MacBook-Air.local>
2021-10-03 16:27:14 +0500

    scroll to top added

--- lib/core/services/api-services.dart
+++ lib/core/services/api-services.dart
@@ -8,15 +8,15 @@ class ApiServices {
     dio.options.headers['Content-Type'] = 'application/json';
     dio.options.headers["accept"] = 'application/json';
 //if the request need token in bearer only then uncomment this
-    if (accessToken != null)
-      dio.options.headers["Authorization"] = 'Bearer $accessToken';
-    dio.options.followRedirects = false;
-    dio.options.validateStatus = (s) {
-      if (s != null)
-        return s < 500;
-      else
-        return false;
-    };
+    // if (accessToken != null)
+    //   dio.options.headers["Authorization"] = 'Bearer $accessToken';
+    // dio.options.followRedirects = false;
+    // dio.options.validateStatus = (s) {
+    //   if (s != null)
+    //     return s < 500;
+    //   else
+    //     return false;
+    // };
 
     return dio;
   }
--- lib/ui/screens/contributors-screen.dart
+++ lib/ui/screens/contributors-screen.dart
@@ -8,76 +8,127 @@ import 'package:hacktoberfest_flutter/ui/widgets/custom_list-tile-front.dart';
 import 'package:provider/provider.dart';
 import '../../core/constants/contributors.dart';
 import 'package:flip_card/flip_card.dart';
-import 'package:get/get.dart';
 
-class Contributors extends StatelessWidget {
+class Contributors extends StatefulWidget {
+  @override
+  _ContributorsState createState() => _ContributorsState();
+}
+
+class _ContributorsState extends State<Contributors> {
+  bool _showBackToTopButton = false;
+
+  ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController()
+      ..addListener(() {
+        setState(() {
+          if (_scrollController.offset >= 400) {
+            _showBackToTopButton = true; // show the back-to-top button
+          } else {
+            _showBackToTopButton = false; // hide the back-to-top button
+          }
+        });
+      });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose(); // dispose the controller
+    super.dispose();
+  }
+
+  // This function is triggered when the user presses the back-to-top button
+  void _scrollToTop() {
+    _scrollController.animateTo(0,
+        duration: Duration(seconds: 1), curve: Curves.linear);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: NestedScrollView(
-          headerSliverBuilder:
-              (BuildContext context, bool innerBoxIsScrolled) => <Widget>[
-            SliverAppBar(
-              backgroundColor: Colors.white,
-              leading: BackButton(
-                color: Color(0xfff74700),
-                onPressed: () => RestartWidget.restartApp(context),
-              ),
-              pinned: true,
-              expandedHeight: 220.0,
-              flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                title: Text(
-                  "Hacktoberfest Contributors",
-                  style: TextStyle(
-                    color: Color(0xfff74700),
-                    fontSize: 17.0,
-                    fontWeight: FontWeight.w600,
-                  ),
+      backgroundColor: Colors.white,
+
+      ///
+      ///[body] of scaffold
+      ///
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) =>
+            <Widget>[
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            leading: BackButton(
+              color: Color(0xfff74700),
+              onPressed: () => RestartWidget.restartApp(context),
+            ),
+            pinned: true,
+            expandedHeight: 220.0,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text(
+                "Hacktoberfest Contributors",
+                style: TextStyle(
+                  color: Color(0xfff74700),
+                  fontSize: 17.0,
+                  fontWeight: FontWeight.w600,
                 ),
-                background: Center(
-                  child: Image.asset(
-                    'assets/banner2021.png',
-                    height: 180.0,
-                  ),
+              ),
+              background: Center(
+                child: Image.asset(
+                  'assets/banner2021.png',
+                  height: 180.0,
                 ),
               ),
             ),
-          ],
-          body: gitHubUserNames.length == contributorNames.length
-              ? ListView.separated(
-                  physics: BouncingScrollPhysics(),
-                  padding: const EdgeInsets.all(8.0),
-                  separatorBuilder: (context, index) =>
-                      Divider(color: Colors.transparent),
-                  itemCount: contributorNames.length,
-                  itemBuilder: (context, index) {
-                    return ContributorFlipCard(index);
-                  },
-                )
-              : Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 18.0, vertical: 80),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Text(
-                          "you forgot to one of required data field i:e username/contributorName",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Color(0xfff74700),
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.w400,
-                          ),
+          ),
+        ],
+        body: gitHubUserNames.length == contributorNames.length
+            ? ListView.separated(
+                controller: _scrollController,
+                physics: BouncingScrollPhysics(),
+                padding: const EdgeInsets.all(8.0),
+                separatorBuilder: (context, index) =>
+                    Divider(color: Colors.transparent),
+                itemCount: contributorNames.length,
+                itemBuilder: (context, index) {
+                  return ContributorFlipCard(index);
+                },
+              )
+            : Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 18.0, vertical: 80),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "you forgot to one of required data field i:e username/contributorName",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color(0xfff74700),
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w400,
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-        ));
+              ),
+      ),
+
+      ///
+      ///[FAB] Scroll To Top Button
+      ///
+      floatingActionButton: _showBackToTopButton == false
+          ? null
+          : FloatingActionButton(
+              onPressed: _scrollToTop,
+              child: Icon(Icons.arrow_upward),
+            ),
+    );
   }
 }
 
@@ -91,9 +142,7 @@ class ContributorFlipCard extends StatefulWidget {
 
 class _ContributorFlipCardState extends State<ContributorFlipCard> {
   GlobalKey<FlipCardState> cardKey = GlobalKey<FlipCardState>();
-
   GithubUser githubUser;
-
   ViewState viewState = ViewState.loading;
 
   @override
@@ -104,7 +153,7 @@ class _ContributorFlipCardState extends State<ContributorFlipCard> {
         onFlipDone: (bool) async {
           if (!bool) {
             print("$bool  getData");
-            model.errorMsg = null;
+            // model.errorMsg = null;
             githubUser = null;
             viewState = ViewState.loading;
             try {
@@ -113,8 +162,10 @@ class _ContributorFlipCardState extends State<ContributorFlipCard> {
               viewState = ViewState.idle;
               setState(() {});
             } catch (e) {
-              viewState = ViewState.idle;
-              setState(() {});
+              WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+                viewState = ViewState.idle;
+                setState(() {});
+              });
             }
           }
         },
