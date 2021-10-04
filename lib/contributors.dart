a847eb36e6b608b22e8958b9246cbfb60777dcbc
samitkapoor <samitkapoor77@gmail.com>
2021-10-04 20:42:00 +0530

    added myself

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -7,6 +7,7 @@ class Contributor extends StatelessWidget {
   // Add your Full Name in this list
   // Please do not remove any other name :)
   final contributorsName = [
+    'Samit Kapoor',
     'Dev Adnani',
     'Muhammad Hamza',
     'Garima Chandna',
@@ -97,6 +98,7 @@ class Contributor extends StatelessWidget {
   ];
 
   final gitHubUserName = [
+    'samitkapoor',
     'Dev-Adnani',
     'mhmzdev',
     'garimachandna',
