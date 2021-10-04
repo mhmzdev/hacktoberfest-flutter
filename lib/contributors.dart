6a6cd3c2a87d84984b0399a179ed905eac163ba0
Akhil Kumar <50401848+ce-akhil@users.noreply.github.com>
2021-10-04 15:44:32 +0530

    Update contributors.dart

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -93,7 +93,8 @@ class Contributor extends StatelessWidget {
     'Neha Ajith',
     'Naveena Ramesh Vardhini ',
     'Muhammad Hilmy AL Jauzy',
-    'Pranav Nair'
+    'Pranav Nair',
+    'Akhil'
   ];
 
   final gitHubUserName = [
@@ -183,6 +184,7 @@ class Contributor extends StatelessWidget {
     'naveeramesh',
     'hilmykun',
     'PranavNair01',
+    'ce-akhil'
   ];
 
   @override
