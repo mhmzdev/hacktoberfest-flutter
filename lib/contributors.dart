29e04688af69e2ce290f375a2e8edfbe6ae4d513
Nimish2000 <nimishbajaj88@gmail.com>
2021-10-05 17:56:20 +0530

    added myself

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -95,7 +95,8 @@ class Contributor extends StatelessWidget {
     'Naveena Ramesh Vardhini ',
     'Muhammad Hilmy AL Jauzy',
     'Pranav Nair',
-    'Aditya Verma'
+    'Aditya Verma',
+    'Nimish Bajaj',
   ];
 
   final gitHubUserName = [
@@ -186,7 +187,8 @@ class Contributor extends StatelessWidget {
     'naveeramesh',
     'hilmykun',
     'PranavNair01',
-    'vermastra'
+    'vermastra',
+    'Nimish2000',
   ];
 
   @override
---
+++ participants/Nimish2000.md
@@ -0,0 +1,3 @@
+Name: Nimish Bajaj
+GitHub: Nimish2000
+Quote: Trust the process,outcome will be good
