5ded74fdd43579876fbd91dc707a3ab10957c3b7
jushutch <jushutch@umich.edu>
2021-10-04 22:14:46 -0400

    Added jushutch

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -95,7 +95,8 @@ class Contributor extends StatelessWidget {
     'Naveena Ramesh Vardhini ',
     'Muhammad Hilmy AL Jauzy',
     'Pranav Nair',
-    'Aditya Verma'
+    'Aditya Verma',
+    'Justin Hutchins',
   ];
 
   final gitHubUserName = [
@@ -186,7 +187,8 @@ class Contributor extends StatelessWidget {
     'naveeramesh',
     'hilmykun',
     'PranavNair01',
-    'vermastra'
+    'vermastra',
+    'jushutch',
   ];
 
   @override
