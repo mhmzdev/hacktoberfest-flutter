a4abb8d6b767c60b47e7a3dc8a1d134e07b63b63
IvanitiX <TheIvanitic20@gmail.com>
2021-10-14 23:33:20 +0200

    [Code] Added item in contributors and Markdown file with the name

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -110,6 +110,7 @@ class Contributor extends StatelessWidget {
     'Prajjwal mehta',
     'Saif M. Al-Ansari',
     'Jacob Ranjit',
+    'Ivan V.R',
   ];
 
   final gitHubUserName = [
@@ -215,6 +216,7 @@ class Contributor extends StatelessWidget {
     'PRAJJWALmehta',
     'Saif2nsari',
     'Jcupzz',
+    'IvanitiX',
   ];
 
   @override
