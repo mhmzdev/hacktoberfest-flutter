3bad9b978549360d13664512e444bcc90b4f91b7
Prahen parija <54349939+prahenstark@users.noreply.github.com>
2021-10-18 23:11:51 +0530

    added my name (Prahen Parija)

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -112,6 +112,7 @@ class Contributor extends StatelessWidget {
     'Jai Aravindh',
     'Jacob Ranjit',
     'Ivan V.R',
+    'Prahen Parija',
   ];
 
   final gitHubUserName = [
@@ -219,6 +220,7 @@ class Contributor extends StatelessWidget {
     'JaiAravindh-git',
     'Jcupzz',
     'IvanitiX',
+    'prahenstark',
   ];
 
   @override
