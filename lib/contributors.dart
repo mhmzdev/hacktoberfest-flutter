83bb1888eeb136e38af15013be3ab7d9cdc18ee8
Arun Thacharuthodi <67404902+arunthacharuthodi@users.noreply.github.com>
2021-10-19 20:03:09 +0530

    added arunthacharithodi

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -112,6 +112,7 @@ class Contributor extends StatelessWidget {
     'Jai Aravindh',
     'Jacob Ranjit',
     'Ivan V.R',
+    'Arun Thacharuthodi'
   ];
 
   final gitHubUserName = [
@@ -219,6 +220,7 @@ class Contributor extends StatelessWidget {
     'JaiAravindh-git',
     'Jcupzz',
     'IvanitiX',
+    'arunthacharuthodi'
   ];
 
   @override
