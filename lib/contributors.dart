1aead91dc53baa87cb6db73cbbf0cd28cc89dbfa
taufiqjack <taufiqdwic@gmail.com>
2023-10-03 10:18:10 +0700

    added myself

--- lib/contributors.dart
+++ lib/contributors.dart
@@ -112,6 +112,7 @@ class Contributor extends StatelessWidget {
     'Jai Aravindh',
     'Jacob Ranjit',
     'Ivan V.R',
+    'Taufiq Dwi Cahyono'
   ];
 
   final gitHubUserName = [
@@ -219,6 +220,7 @@ class Contributor extends StatelessWidget {
     'JaiAravindh-git',
     'Jcupzz',
     'IvanitiX',
+    'taufiqjack'
   ];
 
   @override
---
+++ participants/participant/taufiqjack.md
@@ -0,0 +1,3 @@
+Name: Taufiq Dwi Cahyono
+GitHub: taufiqjack
+Quote: "Don't be tired to learn, because success starts with failure"
